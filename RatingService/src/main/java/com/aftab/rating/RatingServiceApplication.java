04d1307ec1d0c9732c0e6bd6500b7199f0b9e883
aftabalam507 <aftabalam1996jan@gmail.com>
2024-01-14 12:24:12 +0530

    UserService communicate with RatingService with HardCoded URL

--- RatingService/src/main/java/com/aftab/rating/RatingServiceApplication.java
+++ RatingService/src/main/java/com/aftab/rating/RatingServiceApplication.java
@@ -2,6 +2,8 @@ package com.aftab.rating;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class RatingServiceApplication {
@@ -9,5 +11,5 @@ public class RatingServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RatingServiceApplication.class, args);
 	}
-
+	
 }
--- UserService/src/main/java/com/aftab/service/impl/UserServiceImpl.java
+++ UserService/src/main/java/com/aftab/service/impl/UserServiceImpl.java
@@ -1,21 +1,32 @@
 package com.aftab.service.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import com.aftab.entity.Rating;
 import com.aftab.entity.User;
 import com.aftab.exception.ResourceNotFoundException;
 import com.aftab.repository.UserRepo;
 import com.aftab.service.UserService;
+
+import ch.qos.logback.classic.Logger;
 
 @Service
 public class UserServiceImpl implements UserService {
 
 	@Autowired
 	private UserRepo userRepo;
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	private org.slf4j.Logger logger=LoggerFactory.getLogger(UserService.class);
 
 	@Override
 	public User createUser(User user) {
@@ -34,9 +45,15 @@ public class UserServiceImpl implements UserService {
 	@Override
 	public User getUser(String userId) {
 
-		return this.userRepo.findById(userId)
+		User user = this.userRepo.findById(userId)
 				.orElseThrow(() -> new ResourceNotFoundException("User with given id is not find !! : " + userId));
-
+		//fetching rating from rating service
+		//http://localhost:8083/ratings/user/80c56697-2b63-4b55-b5c1-57de9d549d7a
+		
+		ArrayList<Rating> ratingsOfUser = restTemplate.getForObject("http://localhost:8083/ratings/user/"+user.getUserId(), ArrayList.class);
+		logger.info("{} ",ratingsOfUser);
+		user.setRatings(ratingsOfUser);
+		return user;
 	}
 
 	@Override
